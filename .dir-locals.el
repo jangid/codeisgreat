90835c3ef6822ace9031605b3b8baba2314119b2
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-10-31 16:09:46 +0530

    status update

---
+++ .dir-locals.el
@@ -0,0 +1,1 @@
+((org . (org-html-postamble-format . (("en" "<hr/><p class=\"author\">Author: %a (%e)</p><p class=\"date\">Last updated at: %d</p><p class=\"creator\">%c</p><p class=\"validation\">%v</p>")))))
