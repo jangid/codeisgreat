aa6cad471570b5c31cfa4c6c3b48a9f44f33eb2e
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-02 17:48:24 +0530

    ampersand exp

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode
-  (org-html-postamble-format . (("en" "<p class=\"date\">Last updated: %C</p><p class=\"creator\">Created using: %c</p><p class=\"validation\"><a href=\"http://validator.w3.org/check?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F\"><img src=\"http://www.w3.org/Icons/valid-xhtml10\" alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" /></a><a href=\"http://jigsaw.w3.org/css-validator/validator?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=\"><img style=\"border:0;width:88px;height:31px\" src=\"https://jigsaw.w3.org/css-validator/images/vcss\" alt=\"Valid CSS!\" /></a></p>")))
+  (org-html-postamble-format . (("en" "<p class=\"date\">Last updated: %C</p><p class=\"creator\">Created using: %c</p><p class=\"validation\"><a href=\"http://validator.w3.org/check?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F\"><img src=\"http://www.w3.org/Icons/valid-xhtml10\" alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" /></a><a href=\"http://jigsaw.w3.org/css-validator/validator?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F%%26profile=css3svg%%26usermedium=all%%26warning=1%%26vextwarning=\"><img style=\"border:0;width:88px;height:31px\" src=\"https://jigsaw.w3.org/css-validator/images/vcss\" alt=\"Valid CSS!\" /></a></p>")))
   (org-html-postamble . t)))
 
