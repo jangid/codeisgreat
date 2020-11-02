f6eb0f3a5c0ca63e050f4ca88084276079495982
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-02 17:11:27 +0530

    dir-locals working now

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,6 @@
-((org . (org-html-postamble-format . (("en" "<hr/><p class=\"author\">Author: %a (%e)</p><p class=\"date\">Last updated at: %d</p><p class=\"creator\">%c</p><p class=\"validation\">%v</p>")))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-html-postamble-format . (("en" "<p class=\"date\">Last updated: %C</p><p class=\"creator\">Created using: %c</p><p class=\"validation\">%v</p>")))
+  (org-html-postamble . t)))
