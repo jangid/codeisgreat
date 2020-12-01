f4b76640efbcc70238a9e38f45c7d4aecedc0f38
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-01 12:27:00 +0530

    enabled visual-line-mode in directory

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,5 @@
 
 ((org-mode
   (org-html-postamble-format . (("en" "<p class=\"date\">Last updated: %C</p><p class=\"creator\">Created using: %c</p><p class=\"validation\"><a href=\"http://validator.w3.org/check?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F\"><img src=\"https://www.w3.org/Icons/valid-xhtml10\" alt=\"Valid XHTML 1.0 Strict\" height=\"31\" width=\"88\" /></a><a href=\"http://jigsaw.w3.org/css-validator/validator?uri=https%%3A%%2F%%2Fcodeisgreat.org%%2F&amp;profile=css3svg&amp;usermedium=all&amp;warning=1&amp;vextwarning=\"><img style=\"border:0;width:88px;height:31px\" src=\"https://jigsaw.w3.org/css-validator/images/vcss\" alt=\"Valid CSS!\" /></a></p>")))
-  (org-html-postamble . t)))
-
+  (org-html-postamble . t))
+ (nil . ((mode . visual-line))))
