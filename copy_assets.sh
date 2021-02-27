fcbda4e82e9c5d328c95eeb9cc59d482982d5be2
Pankaj Jangid <pankaj@codeisgreat.org>
2021-02-27 09:05:11 +0530

    reverted to basic css

---
+++ copy_assets.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# npx tailwindcss-cli@latest build src/css/main_src.css -o src/css/main.css
+# npx tailwindcss-cli@latest build src/css/main_src.css -o docs/css/main.css
+
+cp src/css/main.css docs/css/main.css
