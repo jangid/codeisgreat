9a440af9a7413b71e9b07bf7859685112ce692f6
Pankaj Jangid <pankaj@codeisgreat.org>
2022-06-21 19:37:28 +0530

    added an old article to front

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,25 +5,42 @@
 
 ;;; Code:
 
-((org-mode
+((nil . ((fill-column . 80)
+		 (mode . auto-fill)))
+ (org-mode
   .
   ((org-publish-project-alist
     .
-    (("website"
-      :components ("pages" "notes" "images" "other"))
-     ("pages"
+    (("pages"
       :base-directory  "~/work/personal/codeisgreat/src/"
       :publishing-directory "~/work/personal/codeisgreat/docs/"
-      :publishing-function org-html-publish-to-html)
+      :publishing-function org-html-publish-to-html
+      :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\" />"
+      :with-toc nil
+      :html-preamble nil
+      :html-postamble t
+      :html-postamble-format (("en" "<p class=\"creator\">Created with %c</p>"))
+      :section-numbers nil
+      :html-indent t)
      ("notes"
       :base-directory "~/work/personal/codeisgreat/src/notes"
       :publishing-directory "~/work/personal/codeisgreat/docs/notes"
-      :publishing-function org-html-publish-to-html)
+      :publishing-function org-html-publish-to-html
+      :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/main.css\" />"
+      :with-toc nil
+      :html-preamble nil
+      :html-postamble t
+      :html-postamble-format (("en" "<p class=\"creator\">Created with %c</p>"))
+      :section-numbers nil
+      :html-indent t)
      ("images"
       :base-directory "~/work/personal/codeisgreat/src/images"
+      :base-extension "jpeg\\|jpg\\|gif\\|png"
       :publishing-directory "~/work/personal/codeisgreat/docs/images"
       :publishing-function org-publish-attachment)
      ("other"
       :base-directory "~/work/personal/codeisgreat/src/other"
       :publishing-directory "~/work/personal/codeisgreat/docs/other"
-      :publishing-function org-publish-attachment))))))
+      :publishing-function org-publish-attachment)
+     ("website"
+      :components ("pages" "notes" "images" "other")))))))
